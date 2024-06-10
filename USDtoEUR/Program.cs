54737570ef3a23d6bb935c1ef864ec9c7ce8aebe
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-10 22:11:10 +0300

    Initial commit

---
+++ USDtoEUR/Program.cs
@@ -0,0 +1,11 @@
+﻿class USDtoEUR
+{
+    static void Main(string[] args)
+
+    {
+        double dollar = double.Parse(Console.ReadLine());
+        double euro = dollar * 0.88;
+        Console.WriteLine($"{euro:F2}");
+
+    }
+}
